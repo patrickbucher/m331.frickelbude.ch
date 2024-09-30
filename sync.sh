1d8a728ffcd144a33b4e6da57af731cb9da8d12e
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-30 21:23:24 +0200

    working sync script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-# TODO: deployment!
-# rsync -av public/ --exclude 'js/mathjax' --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m331.frickelbude.ch/public/
+rsync -av public/ --exclude 'js/mathjax' --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m331.frickelbude.ch/public/

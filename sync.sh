f2e06219e0f0b26b74c9caebb7a5cc0ae0efc84e
Patrick Bucher <patrick.bucher@mailbox.org>
2024-11-18 17:09:18 +0100

    build site in sync script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -av public/ --exclude 'js/mathjax' --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m331.frickelbude.ch/public/
+hugo && rsync -av public/ --exclude 'js/mathjax' --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m331.frickelbude.ch/public/
